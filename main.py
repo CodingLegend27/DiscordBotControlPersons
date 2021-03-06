c40ccf496c011f3d9c65e7a905d22f253bb35ef2
CodingLegend27 <waffler.christoph@gmail.com>
2021-03-06 14:49:44 +0100

    resolve bug for deleting a auto-generated message

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def on_message(message):
         # await on_message(cnt)
 
     
-    if (message.content[0]=='!'): await message.edit(delete_after=6.942)
+    if (message.content[0]=='!' or message.author.bot==True): await message.edit(delete_after=6.942)
     
     
     
